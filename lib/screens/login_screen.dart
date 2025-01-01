1d033874ff7ba2a94da0a19079d0420656522b70
naaasruul <nasrulhaqhidayat@gmail.com>
2025-01-01 11:57:23 +0800

    lgoin

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/widgets/myButton.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = 'login_screen';
@@ -10,6 +12,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +40,10 @@ class _LoginScreenState extends State<LoginScreen> {
             TextField(
               keyboardType: TextInputType.emailAddress,
               textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration:
                   kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
@@ -46,19 +55,41 @@ class _LoginScreenState extends State<LoginScreen> {
               keyboardType: TextInputType.emailAddress,
               textAlign: TextAlign.center,
               obscureText: true,
+              style: TextStyle(color: Colors.black),
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password'),
+                  hintText: 'Enter your password',
+              ),
             ),
             SizedBox(
               height: 24.0,
             ),
             MyButton(
                 buttonColor: Colors.lightBlueAccent,
-                onPress: () {
+                onPress: () async{
                   //Implement login functionality.
+                  try{
+                    final cred = await _auth.signInWithEmailAndPassword(
+                      email:email,
+                      password: password,
+                    );
+
+                    if(cred != null){
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }else{
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+
+                  } on FirebaseAuthException catch(e){
+                    if (e.code == 'user-not-found') {
+                      print('No user found for that email.');
+                    } else if (e.code == 'wrong-password') {
+                      print('Wrong password provided for that user.');
+                    }
+                  }
                 },
                 text: 'Log In')
           ],
